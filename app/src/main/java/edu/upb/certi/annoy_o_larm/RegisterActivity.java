82eb6b4205d059fd9230f765f268189372b4dee1
Apita49 <jose.apa4996@gmail.com>
2017-12-18 14:03:14 -0400

    RegisterActivity

--- app/src/main/java/edu/upb/certi/annoy_o_larm/RegisterActivity.java
+++ app/src/main/java/edu/upb/certi/annoy_o_larm/RegisterActivity.java
@@ -1,50 +1,82 @@
 package edu.upb.certi.annoy_o_larm;
 
-import android.app.Application;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
-    private EditText email;
-    private EditText password;
-    private EditText passwordRep;
-    private Button register;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class RegisterActivity extends AppCompatActivity {
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText passwordRepEditText;
+    private Button registerButton;
+
+    private ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        email = findViewById(R.id.emailEditText);
-        password = findViewById(R.id.passwordEditText);
-        passwordRep = findViewById(R.id.passwordRepEditText);
-        register = findViewById(R.id.registerButton);
-        register.setOnClickListener(new View.OnClickListener() {
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        passwordRepEditText = findViewById(R.id.passwordRepEditText);
+        registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            String mail = email.getText().toString();
-            String pass = password.getText().toString();
-            String passRep = passwordRep.getText().toString();
-            if(pass.equals(passRep)){
-                registroUsuario(mail,password);
-                goToLogin();
-            }else {
-                Toast.makeText(getApplicationContext(), "Sus contraseñas no coinciden", Toast.LENGTH_SHORT).show();
-            }
+                register();
             }
         });
+        progressBar = findViewById(R.id.progressBar);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
-    private void registroUsuario(String mail, EditText password) {
-        //TODO
+    private void register() {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String rPassword = passwordRepEditText.getText().toString();
+        if(!email.equals("")&& !password.equals("") && !rPassword.equals("")) {
+            if (password.equals(rPassword)) {
+                progressBar.setVisibility(View.VISIBLE);
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    goToLogin();
+                                } else {
+                                    showErrorMessage(task.getException().toString());
+                                }
+                            }
+                        });
+            } else {
+                showErrorMessage("Las contraseñas no coinciden");
+            }
+        }else{
+            showErrorMessage("Llene todos los campos por favor");
+        }
     }
 
     private void goToLogin() {
-        //TODO
+        finish();
+    }
+
+    private void showErrorMessage(String s) {
+        Toast.makeText(getApplicationContext(), "Hay error: " + s, Toast.LENGTH_LONG).show();
     }
 }
